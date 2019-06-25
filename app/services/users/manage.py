d2e81078afc10551f8b9b47fd02d901c954d52f4
Terence Then <chenjienan2009@gmail.com>
2019-06-25 01:49:29 -0700

    set up database

--- app/services/users/manage.py
+++ app/services/users/manage.py
@@ -1,9 +1,15 @@
 #!/usr/local/bin/python3
 
 from flask.cli import FlaskGroup
-from project import app
+from project import app, db
 
 cli = FlaskGroup(app)
+
+@cli.command('recreate_db')
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
 
 # run and manage the app from the command line
 if __name__ == '__main__':

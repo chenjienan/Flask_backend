77c0649ee088cade90e590d84d7a10f4185699fa
Terence Then <chenjienan2009@gmail.com>
2019-06-24 21:01:24 -0700

    create a new project

---
+++ app/services/users/manage.py
@@ -0,0 +1,12 @@
+from flask.cli import FlaskGroup
+from project import app
+
+cli = FlaskGroup(app)
+
+# run and manage the app from the command line
+if __name__ == '__main__':
+    cli()
+
+
+# (env)$ export FLASK_APP=project/__init__.py
+# (env)$ python manage.py run
---
+++ app/services/users/project/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+from flask_restful import Resource, Api
+
+
+app = Flask(__name__)
+api = Api(app)
+
+# set config
+app.config.from_object('project.config.DevelopmentConfig')
+
+class UsersPing(Resource):
+    def get(self):
+        return {
+            'status': 'success',
+            'message': 'pong!'
+        }
+
+# config the first route
+api.add_resource(UsersPing, '/users/ping')

e42eceb950f9618c150dd44d7d41be2f6a77c53a
Terence Chen <chenjienan2009@gmail.com>
2019-06-26 09:22:34 -0700

    add flask blueprints

--- app/services/users/manage.py
+++ app/services/users/manage.py
@@ -1,15 +1,30 @@
 #!/usr/local/bin/python3
 
-from flask.cli import FlaskGroup
-from project import app, db
+import sys
+import unittest
 
-cli = FlaskGroup(app)
+from flask.cli import FlaskGroup
+
+from project import create_app, db
+from project.api.models import User
+
+app = create_app()
+cli = FlaskGroup(create_app=create_app)
 
 @cli.command('recreate_db')
 def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
+
+@cli.command()
+def test():
+    """Runs the tests without code coverage"""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    sys.exit(result)
 
 # run and manage the app from the command line
 if __name__ == '__main__':
--- app/services/users/project/__init__.py
+++ app/services/users/project/__init__.py
@@ -1,39 +1,32 @@
 import os
-from flask import Flask, jsonify
-from flask_restful import Resource, Api
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-# import sys
-
-app = Flask(__name__)
-api = Api(app)
-
-# set config
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings)
-# print(app.config, file=sys.stderr)
-
-# instantiate the db
-db = SQLAlchemy(app) 
-
-# model
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
-
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
 
 
-class UsersPing(Resource):
-    def get(self):
-        return {
-            'status': 'success',
-            'message': 'pong!'
-        }
+db = SQLAlchemy()
 
-# config the first route
-api.add_resource(UsersPing, '/users/ping')
+def create_app(script_info=None):
+    # init app
+    app = Flask(__name__)
+
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+
+    # instantiate the db
+    db.init_app(app) 
+
+    # register blueprint    
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    # shell context for flask cli
+    # register the app and db to the shell. Now we can work with 
+    # the application context and the database without having to 
+    # import them directly into the shell
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db }
+    
+
+    return app

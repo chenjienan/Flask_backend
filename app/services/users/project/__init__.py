d8bad03022b50eb1d7db47166c008870415812b0
Terence Then <chenjienan2009@gmail.com>
2019-06-29 14:01:00 -0700

    format code

--- app/services/users/project/__init__.py
+++ app/services/users/project/__init__.py
@@ -5,28 +5,29 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 def create_app(script_info=None):
     # init app
     app = Flask(__name__)
 
     # set config
-    app_settings = os.getenv('APP_SETTINGS')
+    app_settings = os.getenv("APP_SETTINGS")
     app.config.from_object(app_settings)
 
     # instantiate the db
-    db.init_app(app) 
+    db.init_app(app)
 
-    # register blueprint    
+    # register blueprint
     from project.api.users import users_blueprint
+
     app.register_blueprint(users_blueprint)
 
     # shell context for flask cli
-    # register the app and db to the shell. Now we can work with 
-    # the application context and the database without having to 
+    # register the app and db to the shell. Now we can work with
+    # the application context and the database without having to
     # import them directly into the shell
     @app.shell_context_processor
     def ctx():
-        return {'app': app, 'db': db }
-    
+        return {"app": app, "db": db}
 
     return app
--- app/services/users/project/api/users.py
+++ app/services/users/project/api/users.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, render_template
 from flask_restful import Resource, Api
 
 from project import db
@@ -6,53 +6,64 @@ from project.api.models import User
 
 from sqlalchemy import exc
 
-users_blueprint = Blueprint('users', __name__)
+users_blueprint = Blueprint("users", __name__, template_folder="./templates")
 api = Api(users_blueprint)
+
+
+@users_blueprint.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        username = request.form["username"]
+        email = request.form["email"]
+        db.session.add(User(username=username, email=email))
+        db.session.commit()
+
+    users = User.query.all()
+    # it's like a view bag
+    return render_template("index.html", users=users)
+
 
 class Users(Resource):
     def get(self, user_id):
 
-        response = {
-            'status': 'fail',
-            'message': 'User does not exist'
-        }
+        response = {"status": "fail", "message": "User does not exist"}
         try:
             user = User.query.filter_by(id=int(user_id)).first()
-            if not user: return response, 404
-            
+            if not user:
+                return response, 404
+
             response = {
-                'status': 'success',
-                'data': {
-                    'id': user.id,
-                    'username': user.username,
-                    'email': user.email,
-                    'active': user.active
-                }
+                "status": "success",
+                "data": {
+                    "id": user.id,
+                    "username": user.username,
+                    "email": user.email,
+                    "active": user.active,
+                },
             }
             return response, 200
 
         except ValueError:
             return response, 404
-        
+
         response = {
-            'status': 'success',
-            'data': {
-                'id': user.id,
-                'username': user.username,
-                'email': user.email,
-                'active': user.active
-            }
+            "status": "success",
+            "data": {
+                "id": user.id,
+                "username": user.username,
+                "email": user.email,
+                "active": user.active,
+            },
         }
         return response, 200
+
 
 class UsersList(Resource):
     def get(self):
         """Get all users"""
         response = {
-            'status': 'success',
-            'data': {
-                'users': [user.to_json() for user in User.query.all()]
-            }
+            "status": "success",
+            "data": {"users": [user.to_json() for user in User.query.all()]},
         }
         return response, 200
 
@@ -60,16 +71,14 @@ class UsersList(Resource):
         # where is request? It's from Flask request
         post_data = request.get_json()
 
-        response = {
-            'status': 'fail',
-            'message': 'Invalid payload.'
-        }
+        response = {"status": "fail", "message": "Invalid payload."}
 
         # error handling: if data is None
-        if not post_data: return response, 400
+        if not post_data:
+            return response, 400
 
-        username = post_data.get('username')
-        email = post_data.get('email')
+        username = post_data.get("username")
+        email = post_data.get("email")
 
         # error handling: if email exsits
         try:
@@ -78,11 +87,11 @@ class UsersList(Resource):
             if not user:
                 db.session.add(User(username=username, email=email))
                 db.session.commit()
-                response['status'] = 'success'
-                response['message'] = f'{email} was added!'
+                response["status"] = "success"
+                response["message"] = f"{email} was added!"
                 return response, 201
             else:
-                response['message'] = 'Email already exists.'
+                response["message"] = "Email already exists."
                 return response, 400
 
         # what is exc? It's SQL exception
@@ -90,13 +99,12 @@ class UsersList(Resource):
             db.session.rollback()
             return response, 400
 
+
 class UsersPing(Resource):
     def get(self):
-        return {
-            'status': 'success',
-            'message': 'pong'
-        }
+        return {"status": "success", "message": "pong"}
 
-api.add_resource(UsersPing, '/users/ping')
-api.add_resource(UsersList, '/users')
-api.add_resource(Users, '/users/<user_id>')
+api.add_resource(UsersPing, "/users/ping")
+api.add_resource(UsersList, "/users")
+api.add_resource(Users, "/users/<user_id>")
+
